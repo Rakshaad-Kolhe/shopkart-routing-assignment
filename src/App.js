6b69106947c6d706cc63552f9b0b9f1a52511766
Rakshaad-Kolhe <rakshaadkolhe@gmail.com>
2026-05-08 15:10:34 +0530

    Completed ShopKart Routing Assignment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    
       <div>
         <Navbar />
         <Routes>
@@ -16,7 +16,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    
   );
 }
 
